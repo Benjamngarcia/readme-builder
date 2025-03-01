589982f02f207e40e22ba368a2c9b0bea98d373a
Benjamngarcia <bperezg1800@alumno.ipn.mx>
2025-02-28 20:08:18 -0600

    Add github repo button

--- src/components/ui/Navbar/index.tsx
+++ src/components/ui/Navbar/index.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React from "react";
+import { IconStar } from "@tabler/icons-react";
 
 const Navbar: React.FC = () => {
   return (
@@ -6,10 +7,25 @@ const Navbar: React.FC = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
-            <a href="#" className="text-2xl font-semibold">Readme Builder</a>
+            <a href="#" className="text-2xl font-semibold">
+              Readme Builder
+            </a>
           </div>
 
           {/* TODO: This button will be able later */}
+          <div className="flex space-x-4">
+            <a
+              href="https://github.com/Benjamngarcia/readme-builder"
+              target="_blank"
+              className="px-3 py-1 text-white rounded-md transition-all duration-300 transform hover:bg-gray-500 hover:opacity-50 flex items-center justify-center"
+            >
+              <IconStar
+                size={12}
+                className="mr-2 transition-all duration-300 transform hover:scale-125"
+              />
+              GitHub
+            </a>
+          </div>
           {/* <div className="flex space-x-4">
             <a href="#" className="text-lg hover:text-blue-400 transition-colors">Donate</a>
           </div> */}
