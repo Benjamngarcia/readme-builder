f60fe1f81496584e9c2574bb599f1c707282d099
Benjamngarcia <bperezg1800@alumno.ipn.mx>
2025-03-29 16:54:08 -0600

    Add Templates sections

--- src/components/features/SectionList/index.tsx
+++ src/components/features/SectionList/index.tsx
@@ -1,9 +1,10 @@
 "use client";
 
 import { FC, useState } from "react";
-import { Section } from "../../../types";
+import { Section, Template } from "../../../types";
 import { sectionsListArr } from "../../../utils/sectionsList";
 import DraggableSection from "../../ui/DraggableSection";
+import DraggableTemplate from "../../ui/DraggableTemplate";
 import TextInput from "../../ui/TextInput";
 import Modal from "../../ui/Modal";
 import Button from "../../../components/ui/Button";
@@ -12,9 +13,13 @@ import { availableTechnologies } from "../../../utils/availableTechnologies";
 import { IconArrowLeft } from "@tabler/icons-react";
 import TechnologiesList from "../../../components/ui/TechnologiesList";
 import GithubStats from "../../../components/ui/GithubStats";
+import { templatesList } from "../../../utils/templateList";
 
 const SectionList: FC = () => {
   const { selectedSectionId, setSelectedSectionId } = useEditorContext();
+  const [viewMode, setViewMode] = useState<"sections" | "templates">(
+    "sections"
+  );
   const [searchTerm, setSearchTerm] = useState<string>("");
   const [sections, setSections] = useState<Section[]>(sectionsListArr);
   const [newSectionName, setNewSectionName] = useState<string>("");
@@ -25,8 +30,8 @@ const SectionList: FC = () => {
     section.title.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
-  const filteredTechnologies = availableTechnologies.filter((tech) =>
-    tech.label.toLowerCase().includes(searchTerm.toLowerCase())
+  const filteredTemplates = templatesList.filter((template) =>
+    template.title.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
   const handleSelect = (id: string) => {
@@ -45,7 +50,13 @@ const SectionList: FC = () => {
   const renderEditSections = () => {
     switch (selectedSectionId) {
       case "13":
-        return <TechnologiesList filteredTechnologies={filteredTechnologies} />;
+        return (
+          <TechnologiesList
+            filteredTechnologies={availableTechnologies.filter((tech) =>
+              tech.label.toLowerCase().includes(searchTerm.toLowerCase())
+            )}
+          />
+        );
       case "21":
         return <GithubStats />;
       case "23":
@@ -53,21 +64,28 @@ const SectionList: FC = () => {
       default:
         return (
           <div className="overflow-y-auto max-h-[60vh] space-y-2 pr-4">
-            {filteredSections.map((section) => (
-              <DraggableSection
-                key={section.id}
-                section={section}
-                isSelected={selectedSection === section.id}
-                onSelect={() => handleSelect(section.id)}
-              />
-            ))}
-            <Button
-              onClick={() => setIsModalOpen(true)}
-              variant="colored"
-              color="blue"
-            >
-              + Custom Section
-            </Button>
+            {viewMode === "sections" &&
+              filteredSections.map((section) => (
+                <DraggableSection
+                  key={section.id}
+                  section={section}
+                  isSelected={selectedSection === section.id}
+                  onSelect={() => handleSelect(section.id)}
+                />
+              ))}
+            {viewMode === "templates" &&
+              filteredTemplates.map((template) => (
+                <DraggableTemplate key={template.id} template={template} />
+              ))}
+            {viewMode === "sections" && (
+              <Button
+                onClick={() => setIsModalOpen(true)}
+                variant="colored"
+                color="blue"
+              >
+                + Custom Section
+              </Button>
+            )}
           </div>
         );
     }
@@ -87,21 +105,48 @@ const SectionList: FC = () => {
           Back to Editor
         </Button>
       ) : null}
+
+      {/* Switch entre Sections y Templates */}
+      <div className="inline-flex mb-4">
+        <button
+          onClick={() => setViewMode("sections")}
+          className={`px-4 py-2 border  transition duration-300 ease-in-out rounded-l-full focus:outline-none focus:ring-2 focus:ring-blue-500 ${
+            viewMode === "sections"
+              ? "border-blue-500 text-blue-500"
+              : "border-gray-700 bg-transparent text-white hover:bg-gray-700"
+          }`}
+        >
+          Sections
+        </button>
+        <button
+          onClick={() => setViewMode("templates")}
+          className={`px-4 py-2 border border-l-0 transition duration-300 ease-in-out rounded-r-full focus:outline-none focus:ring-2 focus:ring-blue-500 ${
+            viewMode === "templates"
+              ? "border-blue-500 text-blue-500"
+              : "border-gray-700 bg-transparent text-white hover:bg-gray-700"
+          }`}
+        >
+          Templates
+        </button>
+      </div>
+
       <div className="font-bold text-xl mb-4">
         {selectedSectionId === "13"
           ? "Technologies"
           : selectedSectionId === "21" || selectedSectionId === "23"
           ? "Custom your widget"
-          : "Sections"}
+          : viewMode === "sections"
+          ? "Sections"
+          : "Templates"}
       </div>
       <p className="text-sm text-gray-400">
         {selectedSectionId === "13"
           ? "Select the technologies that you wish to display."
           : selectedSectionId === "21" || selectedSectionId === "23"
           ? "Complete the info to your widget"
-          : "Drag and drop sections to rearrange them."}
+          : "Drag and drop items to arrange your content."}
       </p>
-      {selectedSectionId === "13" || selectedSectionId === null ? (
+      {(selectedSectionId === "13" || selectedSectionId === null) && (
         <div className="my-4">
           <TextInput
             value={searchTerm}
@@ -109,37 +154,41 @@ const SectionList: FC = () => {
             placeholder={
               selectedSectionId === "13"
                 ? "Search Technologies..."
-                : "Search Sections..."
+                : viewMode === "sections"
+                ? "Search Sections..."
+                : "Search Templates..."
             }
           />
         </div>
-      ) : null}
+      )}
 
       {renderEditSections()}
-      <Modal
-        isOpen={isModalOpen}
-        onClose={() => setIsModalOpen(false)}
-        title="Add New Section"
-      >
-        <TextInput
-          value={newSectionName}
-          onChange={(e) => setNewSectionName(e.target.value)}
-          placeholder="Enter section name"
-          id="newSectionName"
-        />
-        <div className="flex justify-between">
-          <Button
-            onClick={() => setIsModalOpen(false)}
-            variant="outlined"
-            color="gray"
-          >
-            Cancel
-          </Button>
-          <Button onClick={handleAddSection} variant="colored" color="blue">
-            Save
-          </Button>
-        </div>
-      </Modal>
+      {viewMode === "sections" && (
+        <Modal
+          isOpen={isModalOpen}
+          onClose={() => setIsModalOpen(false)}
+          title="Add New Section"
+        >
+          <TextInput
+            value={newSectionName}
+            onChange={(e) => setNewSectionName(e.target.value)}
+            placeholder="Enter section name"
+            id="newSectionName"
+          />
+          <div className="flex justify-between">
+            <Button
+              onClick={() => setIsModalOpen(false)}
+              variant="outlined"
+              color="gray"
+            >
+              Cancel
+            </Button>
+            <Button onClick={handleAddSection} variant="colored" color="blue">
+              Save
+            </Button>
+          </div>
+        </Modal>
+      )}
     </div>
   );
 };
